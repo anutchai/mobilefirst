e7f04784245902f7d9802c731342545dc46de2d7
anutchai <anuschai.bsod@gmail.com>
2021-07-27 15:45:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/mobilefirst/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobilefirst
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dart/helloworld.dart
@@ -0,0 +1,4 @@
+void main() {
+  print("Anutchai");
+  print("Chutipascharoen");
+}
